35d5f19b183723b30970041c1d5bd178e075042b
johnmausr-lang <johnmausr@gmail.com>
2025-11-28 13:11:32 +0500

    Create not-found.jsx

---
+++ app/not-found.jsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        padding: 40,
+        textAlign: "center",
+        color: "white",
+        background: "black",
+        minHeight: "100vh",
+      }}
+    >
+      <h1 style={{ fontSize: 32 }}>Страница не найдена</h1>
+      <p>Но я всё равно здесь рядом 💛</p>
+    </div>
+  );
+}
