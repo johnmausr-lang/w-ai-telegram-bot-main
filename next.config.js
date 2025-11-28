dfc0ffbb1c0eb1eba8d699b64d73bc1393ec730d
johnmausr-lang <johnmausr@gmail.com>
2025-11-28 13:06:18 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
-export default { reactStrictMode:false };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
+
+  // Важно для работы API на Node, а не Edge
+  runtime: "nodejs",
+};
+
+export default nextConfig;
