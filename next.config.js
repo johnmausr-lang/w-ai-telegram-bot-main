b6545a4ae53a5ed4a6c78051b789d8a19c747785
johnmausr-lang <johnmausr@gmail.com>
2025-11-28 14:48:15 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
-    serverActions: { allowedOrigins: ["*"] },
-  },
-  runtime: "nodejs",
+    serverActions: {
+      allowedOrigins: ["*"]
+    }
+  }
+  // runtime: "nodejs" ← УДАЛИ ЭТУ СТРОКУ!
 };
 
 export default nextConfig;
